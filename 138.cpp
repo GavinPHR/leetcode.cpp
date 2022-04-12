ad76357b1cd98d96f494efedadcac95db9cd9a2e
Haoran Peng <gavinsweden@gmail.com>
2022-04-12 10:42:05 +0100

    138. Copy List with Random Pointer

---
+++ 138.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> map;
+        Node *cur = head;
+        while (cur) {
+            Node *new_head = new Node(cur->val);
+            map[cur] = new_head;
+            cur = cur->next;
+        }
+        cur = head;
+        while (cur) {
+            map[cur]->next = map[cur->next];
+            map[cur]->random = map[cur->random];
+            cur = cur->next;
+        }
+        return map[head];
+    }
+};

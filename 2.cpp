8e64943ada53dda8c0357533750af0e3fd91affa
Haoran Peng <haoranpeng@meta.com>
2022-11-17 14:25:03 +0000

    2. Add Two Numbers

--- 2.cpp
+++ 2.cpp
@@ -1,72 +1,31 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
- 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        ListNode* prev;
-        ListNode* head = l1;
-        int a, b;
+        auto head = new ListNode(0);
+        auto cur = head;
         while (l1 || l2) {
-            a = l1 ? l1->val : 0;
-            b = l2 ? l2->val : 0;
-            int val = a + b + carry;
-            if (l1) {
-                l1->val = val % 10;
-                prev = l1;
-                l1 = l1->next;
-            } else {
-                prev->next = new ListNode(val % 10);
-                prev = prev->next;
-            }
-            if (l2){
-                l2 = l2->next;
-            }
-            carry = val / 10;
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            cur->next = new ListNode(sum);
+            cur = cur->next;
+            l1 = l1 ? l1->next : l1;
+            l2 = l2 ? l2->next : l2;
         }
         if (carry) {
-            prev->next = new ListNode(1);
+            cur->next = new ListNode(carry);
         }
-        return head;
+        return head->next;
     }
 };
-
-ListNode* vec2list(const vector<int>& v) {
-    ListNode* head = new ListNode();
-    ListNode* cur = head;
-    int i = 0;
-    for (;i < v.size() - 1; ++i) {
-        cur->val = v[i];
-        cur->next = new ListNode();
-        cur = cur->next;
-    }
-    cur->val = v[v.size() - 1];
-    return head;
-}
-
-int main(int argc, char const *argv[]) {
-    Solution s = Solution();
-    vector<int> a = {9,9,9,9,9,9,9};
-    vector<int> b = {9,9,9,9};
-    for (auto i: a) {
-        cout << i;
-    }
-    ListNode* head = s.addTwoNumbers(vec2list(a), vec2list(b));
-    while (head != nullptr) {
-        cout << head->val << " ";
-        head = head->next;
-    }
-    return 0;
-}
